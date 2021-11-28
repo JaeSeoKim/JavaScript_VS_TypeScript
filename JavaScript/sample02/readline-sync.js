b3d6fb33fc1d1fe08888b9dab60d967c51b54d10
JaeSeoKim <devjaeseo@gmail.com>
2021-11-29 03:01:47 +0900

    fix: Update js sample02 readlineQuestionSync

--- JavaScript/sample02/readline-sync.js
+++ JavaScript/sample02/readline-sync.js
@@ -1,13 +1,15 @@
 import readline from "readline";
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+export function readlineQuestionSync(query) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-export const readlineQuestionSync = (query) =>
-  new Promise((resolve) => {
+  return new Promise((resolve) => {
     rl.question(query, (answer) => {
+      rl.close();
       resolve(answer);
     });
   });
+}
